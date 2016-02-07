a03d95fb52c784214dc5564611690c394a9312ca
Quisy <lamprecht.mariusz@gmail.com>
2016-02-07 13:01:56 +0100

    added scripts

---
+++ src/main/resources/js/UserViewModel.js
@@ -0,0 +1,31 @@
+const loginUrl = (login, password) => `/user/login/${login}/pw/${password}`;
+
+
+    function UserViewModel(){
+
+            this.login = ko.observable("bhbhbh");
+            this.password = ko.observable("xxx");
+            this.isLogged = false;
+
+
+            this.loginUser = function(){
+                if (confirm("asfasfas")) {
+                   this.isLogged(true);
+                   alert(this.login());
+                }
+
+                        $.getJSON(loginUrl(this.login(), this.password()), (data) => {
+                            if(data.isSuccess){
+                                //sessionStorage.setItem(password, this.password);
+                                //sessionStorage.setItem(login, this.login);
+                                this.isLogged(true);
+                                window.location.reload();
+                            }
+                        });
+            }
+
+
+        }
+
+
+     ko.applyBindings(new UserViewModel(), document.getElementById("panelLogin"));
